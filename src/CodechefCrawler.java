fa09fbf5cd908593dc61cbfda8a28cbc03650001
flapy <y13uc074@lnmiit.ac.in>
2017-07-26 00:52:01 +0530

    Feature Addition: Partition solutions on the basis of Contest
    For every contest there will be a new folder and the associated submissions for the contest will be in that folder. A separate folder will be made for Practice Questions

--- src/CodechefCrawler.java
+++ src/CodechefCrawler.java
@@ -1,3 +1,4 @@
+import javafx.util.Pair;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -24,20 +25,15 @@ public class CodechefCrawler {
     private final String CODECHEF_URL = "https://www.codechef.com";
     private final int MAX_RETRIES = 5;
 
+
+    private List <Contest> contestList = new ArrayList<>();
+
     private String username;
-    private List<String> problemList;
-    private List<String> problemURLs;
-    private List<String> problemIDs;
-    private List<String> languages;
     private HttpURLConnection conn;
 
     public CodechefCrawler(String username) {
         this.username = username;
         this.conn = null;
-        this.problemList = new ArrayList<>();
-        this.problemURLs = new ArrayList<>();
-        this.problemIDs = new ArrayList<>();
-        this.languages = new ArrayList<>();
     }
 
     /**
@@ -45,6 +41,7 @@ public class CodechefCrawler {
      * 1. Find the problems which are solved by the user.
      * 2. Find the submission ID for an AC solution of each solved problem.
      * 3. Finally, fetch the solutions from the submission IDs.
+     *
      * @throws Exception
      */
     void crawl() throws Exception {
@@ -55,6 +52,7 @@ public class CodechefCrawler {
 
     /**
      * Finds the problems which have been solved by the user on CodeChef.
+     *
      * @throws IOException
      * @throws InterruptedException
      */
@@ -66,7 +64,6 @@ public class CodechefCrawler {
             return;
         }
         Document doc = Jsoup.parse(content);
-
         Elements cells = doc.select("p");
         for (int i = 0; i < cells.size(); i++) {
             Element temp = cells.get(i);
@@ -76,13 +73,18 @@ public class CodechefCrawler {
                 if (typeOfContest.equals("")) {
                     return;
                 }
+                Contest contestObject = new Contest();
                 System.out.println("Contest Type :" + typeOfContest);
+                contestObject.setContestName(typeOfContest);
                 Elements problems = contest.select("a");
                 for (Element problem : problems) {
+                    Problem problemObject = new Problem();
                     System.out.println("Solved:" + problem.text());
-                    problemList.add(problem.text());
-                    problemURLs.add(problem.attr("href"));
+                    problemObject.setProblemCode(problem.text());
+                    problemObject.setProblemURL(problem.attr("href"));
+                    contestObject.getProblemList().add(problemObject);
                 }
+                contestList.add(contestObject);
                 break;
             }
             System.out.println("-----------------------------");
@@ -92,100 +94,119 @@ public class CodechefCrawler {
 
     /**
      * Fetches the submission IDs for AC solutions of a given user.
+     *
      * @throws IOException
      * @throws InterruptedException
      */
-    private void fetchSubmissionIDs() throws IOException, InterruptedException {
-        int tries;
-        String url;
-        boolean found;
-        boolean isHidden;
+    private Pair<String,String> getLanguageAndProblemId(String problemCode, String problemURL) throws IOException, InterruptedException {
+        int tries = 0;
+        boolean found = false;
+        boolean isHidden = false;
+        Pair pair = null;
+        System.out.println("Finding submission ID for problem: " + problemCode);
+        while (!found && !isHidden) {
+            String url = CODECHEF_URL + problemURL + "?page=" + Integer.toString(tries);
+            Document doc = Jsoup.parse(getPageContent(url));
 
-        // Traverse through the problem list.
-        for (int i = 0; i < problemList.size(); i++) {
-            // For each problem:
-            tries = 0;
-            found = false;
-            isHidden = false;
-
-            System.out.println("Finding submission ID for problem: " + problemList.get(i));
-            while (!found && !isHidden) {
-                url = CODECHEF_URL + problemURLs.get(i) + "?page=" + Integer.toString(tries);
-                Document doc = Jsoup.parse(getPageContent(url));
-
-                // For each page of submission for this problem:
-                Elements rows = doc.select("tr[class=\\\"kol\\\"]");
-                for (Element row : rows) {
-                    // For each row, there are 8 fields.
-                    Elements fields = row.select("td");
-                    if (fields.toString().contains("No Recent Activity")) {
-                        System.out.println("This problem has hidden solution or no solution. Skipping...");
-                        isHidden = true;
-                        break;
-                    }
-
-                    // First check if this row is AC.
-                    if (fields.get(3).toString().contains("tick-icon.gif")) {
-                        problemIDs.add(fields.get(0).text());
-                        languages.add(fields.get(6).text());
-                        System.out.println("Found!");
-                        found = true;
-                        break;
-                    }
+            // For each page of submission for this problem:
+            Elements rows = doc.select("tr[class=\\\"kol\\\"]");
+            for (Element row : rows) {
+                // For each row, there are 8 fields.
+                Elements fields = row.select("td");
+                if (fields.toString().contains("No Recent Activity")) {
+                    System.out.println("This problem has hidden solution or no solution. Skipping...");
+                    isHidden = true;
+                    break;
                 }
 
-                tries++;
+                // First check if this row is AC.
+                if (fields.get(3).toString().contains("tick-icon.gif")) {
+                    pair = new Pair(fields.get(0).text(), fields.get(6).text());
+                    System.out.println("Found!");
+                    found = true;
+                    break;
+                }
             }
-            System.out.println("Completed " + (i + 1) + "/" + problemList.size());
-            System.out.println();
+
+            tries++;
+        }
+        return pair;
+    }
+    private void fetchSubmissionIDs() throws IOException, InterruptedException {
+
+        for (Contest contest : contestList) {
+            List<Problem> problemList = contest.getProblemList();
+            for (Problem problem : problemList) {
+                String problemCode = problem.getProblemCode();
+                String problemURL = problem.getProblemURL();
+                Pair <String,String> pair = getLanguageAndProblemId(problemCode, problemURL);
+                problem.setProblemId(pair.getKey());
+                problem.setLanguage(pair.getValue());
+            }
         }
     }
 
     /**
      * Fetches the problem solutions submitted by the user to CodeChef; one AC solution for each problem.
+     *
      * @throws IOException
      * @throws InterruptedException
      */
     private void fetchSolutions() throws IOException, InterruptedException {
+
+
+
         String offset = CODECHEF_URL + "/viewplaintext/";
         System.out.println("Fetching codes:");
         System.out.println("================================");
 
-        for (int i = 0; i < problemIDs.size(); i++) {
-            String url = offset + problemIDs.get(i);
-            String filename = problemList.get(i) + findExtension(languages.get(i));
-            String pageContent = getPageContent(url);
-            if (pageContent == null) {
-                // Private problem situation. Skipping...
-                System.out.println("Unable to fetch solution for problem " + problemList.get(i) + ".");
-                System.out.print(problemList.get(i) + " seems to be a private contest problem. " +
-                        "It can only be fetched with user credentials. Skipping...");
-                continue;
-            }
-            String code = Jsoup.parse(pageContent).select("pre").text();
-
-            String filePath = CODECHEF_DIR + FILE_SEPARATOR + username + FILE_SEPARATOR + filename;
+        for (Contest contest : contestList) {
+            String contestName = contest.getContestName();
+            String filePath = CODECHEF_DIR + FILE_SEPARATOR + username + FILE_SEPARATOR
+                                                                    + contestName.substring(0, contestName.length() - 1);
+            System.out.println(filePath);
             File file = new File(filePath);
-            File parentDirectory = file.getParentFile();
-            if (parentDirectory != null) {
-                parentDirectory.mkdirs();
+            if (file != null) {
+                file.mkdirs();
             }
+            List<Problem> problemList = contest.getProblemList();
+            for (Problem problem : problemList) {
+                String url = offset + problem.getProblemId();
+                String problemCode = problem.getProblemCode();
+                String fileName = problemCode + findExtension(problem.getLanguage());
+                String pageContent = getPageContent(url);
+                System.out.println(fileName);
+                if (pageContent == null) {
+                    System.out.println("Unable to fetch solution for problem " + problemCode + ".");
+                    System.out.print(problemCode + " seems to be a private contest problem. " +
+                            "It can only be fetched with user credentials. Skipping...");
+                    continue;
+                }
+                String innerFilePath = filePath + FILE_SEPARATOR + fileName;
+                file = new File(innerFilePath);
+                File parentDirectory = file.getParentFile();
+                if (parentDirectory != null) {
+                    parentDirectory.mkdirs();
+                }
 
-            FileWriter fileWriter = new FileWriter(file);
-            System.out.println("path=" + filePath);
-            System.out.println("Completed : " + (i + 1) + "/" + problemIDs.size());
-            System.out.println();
+                String code = Jsoup.parse(pageContent).select("pre").text();
+                FileWriter fileWriter = new FileWriter(file);
+                System.out.println("path=" + filePath);
+                System.out.println("Completed : ");
+                System.out.println();
 
-            StringTokenizer tokenizer = new StringTokenizer(code, "\n");
-            while (tokenizer.hasMoreTokens()) {
-                fileWriter.write(tokenizer.nextToken() + "\n");
+                StringTokenizer tokenizer = new StringTokenizer(code, "\n");
+                while (tokenizer.hasMoreTokens()) {
+                    fileWriter.write(tokenizer.nextToken() + "\n");
+                }
+                fileWriter.close();
             }
-            fileWriter.close();
         }
     }
 
     /**
      * Fetches source code of the webpage whose URL is provided.
+     *
      * @param url Link to the webpage whose source page is to be fetched.
      * @return The webpage response for the given URL.
      * @throws IOException
@@ -206,7 +227,7 @@ public class CodechefCrawler {
             System.out.println("Sending GET request to URL: " + url);
             try {
                 responseCode = conn.getResponseCode();
-            } catch(IOException e) {
+            } catch (IOException e) {
                 System.out.println("Oops! Looks like Codechef is hanging again :-).\n Please re-try the command.");
                 return null;
             }
@@ -243,6 +264,7 @@ public class CodechefCrawler {
 
     /**
      * Finds the appropriate file extension for a given input language.
+     *
      * @param lang The programming language whose file extension is to be fetched.
      * @return Required file extension.
      */
